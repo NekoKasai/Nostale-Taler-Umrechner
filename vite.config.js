f905737bb6fd78ad7aa4fffbdef48e9595210493
NekoKasai <nekokasaigames@gmail.com>
2025-10-21 13:23:35 +0200

    Update 0.1.1
    Anpassung der Dateien

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/taler-rechner/',
+  base: './', // Wichtig für Netlify
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
