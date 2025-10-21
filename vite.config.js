c236fa10d22a148a7fe806685f7d91738ccb3ce5
NekoKasai <nekokasaigames@gmail.com>
2025-10-21 13:29:24 +0200

    Update 0.1.2
    git add .
    git commit -m "Fix Vite build: move index.html to root"
    git push

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Wichtig für Netlify
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    emptyOutDir: true
+  },
+  publicDir: 'public'
 })
